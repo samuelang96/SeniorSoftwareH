afc1ccd811023c00e536740209f34eb640b77f6d
samuelang96 <samuel.ang.prog@gmail.com>
2018-03-19 21:36:33 -0500

    Added barcode scanner and http request functionality

---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product {
+	int price;
+	string code;
+	string name;
+
+	public Product(){
+		
+	}
+
+	public Product(string jsonString){
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/RequestsManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class RequestsManager : MonoBehaviour {
+	public Text productReturnText;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetText());
+		GetText ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator GetProduct(string barcode){
+		if (barcode.Length < 13) {
+			barcode = "0" + barcode;
+		}
+		UnityWebRequest www = UnityWebRequest.Get("http://heb.cs.trinity.edu/api/products/" + barcode);
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log(www.downloadHandler.text);
+
+			// Or retrieve results as binary data
+			byte[] results = www.downloadHandler.data;
+		}
+		productReturnText.text = www.downloadHandler.text;
+		Debug.Log ("BARCODE: " + barcode);
+		Debug.Log (www.downloadHandler.text);
+	}
+
+	IEnumerator GetText() {
+		UnityWebRequest www = UnityWebRequest.Get("http://heb.cs.trinity.edu/api/products/" + "0070847012474");
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log(www.downloadHandler.text);
+
+			// Or retrieve results as binary data
+
+			byte[] results = www.downloadHandler.data;
+		}
+	}
+}
